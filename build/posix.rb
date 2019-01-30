965b8fda72731123d9e857be7220d84541741f91
Liam Dawson <liam@ldaws.com>
2019-01-30 15:55:50 +1100

    install rustup

--- build/posix.rb
+++ build/posix.rb
@@ -1,3 +1,6 @@
 kzn_symlink_path = File.join(ENV['HOME'], '.local', 'bin', 'kzn')
 
 File.symlink(File.join(__dir__, '..', 'kzn'), kzn_symlink_path) unless File.exist? kzn_symlink_path
+
+# TODO: don't run if already installed
+system("curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path --default-toolchain stable -y")
