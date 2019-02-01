a78f784edebe00ba2e4dffd58fa74510f9547c97
Liam Dawson <liam@ldaws.com>
2019-02-01 22:57:13 +1100

    fix ubuntu pet warning

--- home/posix/.profile.d/80-pet.sh
+++ home/posix/.profile.d/80-pet.sh
@@ -1,21 +1,25 @@
 #shellcheck shell=bash
 
-if [ -n "$ZSH_VERSION" ]
+# bindings won't work if there's no tty
+if [ -t 1 ]
 then
-  function pet-select() {
-    BUFFER=$(pet search --query "$LBUFFER")
-    CURSOR=$#BUFFER
-    zle redisplay
-  }
-  zle -N pet-select
-  stty -ixon
-  bindkey '^s' pet-select
-elif [ -n "$BASH_VERSION" ]
-then
-  function pet-select() {
-    BUFFER=$(pet search --query "$READLINE_LINE")
-    READLINE_LINE=$BUFFER
-    READLINE_POINT=${#BUFFER}
-  }
-  bind -x '"\C-x\C-r": pet-select'
+  if [ -n "$ZSH_VERSION" ]
+  then
+    function pet-select() {
+      BUFFER=$(pet search --query "$LBUFFER")
+      CURSOR=$#BUFFER
+      zle redisplay
+    }
+    zle -N pet-select
+    stty -ixon
+    bindkey '^s' pet-select
+  elif [ -n "$BASH_VERSION" ]
+  then
+    function pet-select() {
+      BUFFER=$(pet search --query "$READLINE_LINE")
+      READLINE_LINE=$BUFFER
+      READLINE_POINT=${#BUFFER}
+    }
+    bind -x '"\C-x\C-r": pet-select'
+  fi
 fi
