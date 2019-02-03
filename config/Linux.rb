cbe39ef3fe037af81aa09432486c8d75821f4f92
Liam Dawson <liam@ldaws.com>
2019-02-03 13:29:55 +1100

    attempt gpg configuration for Fedora

--- config/Linux.rb
+++ config/Linux.rb
@@ -1,1 +1,2 @@
 require_relative './sub/gnome' if ENV['XDG_SESSION_DESKTOP'] =~ /gnome/i
+require_relative './sub/gpg_systemd'
---
+++ config/sub/gpg_systemd.rb
@@ -0,0 +1,1 @@
+system("systemctl enable --user --now dirmngr.socket gpg-agent.socket gpg-agent-ssh.socket")
