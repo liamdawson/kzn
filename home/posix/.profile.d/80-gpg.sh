cf56cee91cdb6915b7043d5779e0ac13938065f4
Liam Dawson <liam@ldaws.com>
2019-02-12 08:40:44 +1100

    alias gpg to gpg2 if necessary

--- home/posix/.profile.d/80-gpg.sh
+++ home/posix/.profile.d/80-gpg.sh
@@ -1,1 +1,9 @@
 export GPG_TTY="$(tty)"
+
+GPG_PATH="$(command -v gpg 2>/dev/null)"
+GPG2_PATH="$(command -v gpg2 2>/dev/null)"
+
+if [[ ! -z "${GPG2_PATH}" ]] && [[ ! -z "${GPG2_PATH}" ]]
+then
+  alias gpg="${GPG2_PATH}"
+fi
