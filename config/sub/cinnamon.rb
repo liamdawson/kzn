850235a65fb20d827b97d39f928a6a28c909a5e9
Liam Dawson <liam@ldaws.com>
2019-02-15 23:15:02 +1100

    add cinnamon configuration sub

---
+++ config/sub/cinnamon.rb
@@ -0,0 +1,57 @@
+config = lambda do |_|
+  namespace 'org/cinnamon' do
+    namespace 'desktop/interface' do
+      set 'clock-show-date', true
+    end
+
+    namespace 'sounds' do
+      %w[login-enabled logout-enabled unplug-enabled tile-enabled plug-enabled switch-enabled].each do |name|
+        set name, false
+      end
+    end
+
+    namespace 'settings-daemon' do
+      namespace 'peripherals/touchpad' do
+        set 'natural-scroll', false
+        set 'scroll-method', "'two-finger-scrolling'"
+        set 'horiz-scroll-enabled', true
+      end
+
+      set 'plugins/power/button-power', "'interactive'"
+    end
+  end
+end
+
+class CinnamonConfig
+  def initialize
+    @namespace = ''
+  end
+
+  def self.configure(&block)
+    CinnamonConfig.new.instance_eval(&block)
+  end
+
+  # @param name [String]
+  def namespace(name, &block)
+    old_namespace = @namespace
+    @namespace = [old_namespace, name.gsub(%r{(^/|/$)}, '')].join('/')
+
+    instance_eval(&block)
+
+    @namespace = old_namespace
+  end
+
+  # @param name [String]
+  def set(name, new_value)
+    path = if @namespace
+             [@namespace, name.gsub(%r{(^/|/$)}, '')].join('/')
+           else
+             name
+           end
+
+    # format: dconf write /this/is/the/path ('value' | true)
+    system("dconf write '#{path}' \"#{new_value.to_s.gsub(/(^"|"$)/, "'")}\"")
+  end
+end
+
+CinnamonConfig.configure(&config)
