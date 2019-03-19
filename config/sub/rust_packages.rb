b30d804175f58f0574293e5f740e5dcd54459578
Liam Dawson <liam@ldaws.com>
2019-03-19 13:05:51 +1100

    finish adding default rust packages

--- config/sub/rust_packages.rb
+++ config/sub/rust_packages.rb
@@ -1,2 +1,5 @@
+def cargo_binary(name)
+  system("command -v #{name} >/dev/null") || system("cargo install #{name}")
+end
 system('rustup component add rustfmt clippy rust-src rust-analysis rls')
-system('cargo install cargo-tree')
+%w[cargo-tree].each { |name| cargo_binary(name) }
