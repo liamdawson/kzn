3c2e2a5e3f75f628ac19e10274a2f38b1b26e0f0
Liam Dawson <liam@ldaws.com>
2019-03-19 12:23:11 +1100

    add default rust packages

--- config/sub/envs.rb
+++ config/sub/envs.rb
@@ -1,18 +1,21 @@
 installs = [
-  -> () { system("rbenv install -s 2.6.1") },
-  -> () { system("pyenv install -s 3.7.2") },
-  -> () { system("pyenv install -s 2.7.15") },
-  -> () { system("nodenv install -s 10.15.0") },
-  -> () { system("nodenv install -s 11.6.0") },
-  -> () { system("goenv install -s 1.11.4") },
-  -> () { system("rustup update") },
+  -> { system('rbenv install -s 2.6.1') },
+  -> { system('pyenv install -s 3.7.2') },
+  -> { system('pyenv install -s 2.7.15') },
+  -> { system('nodenv install -s 10.15.0') },
+  -> { system('nodenv install -s 11.6.0') },
+  -> { system('goenv install -s 1.11.4') },
+  -> { system('rustup update') },
 ]
 
-if installs.all? {|b| b.call}
-  system("rbenv global 2.6.1")
-  system("pyenv global 3.7.2")
-  system("nodenv global 10.15.0")
-  system("goenv global 1.11.4")
+if installs.all?(&:call)
+  system('rbenv global 2.6.1')
+  system('pyenv global 3.7.2')
+  system('nodenv global 10.15.0')
+  system('goenv global 1.11.4')
+  system('rustup default stable')
+
+  require_relative './rust_packages'
 else
-  puts "some installs failed, not setting globals"
+  puts 'some installs failed, not setting globals'
 end
---
+++ config/sub/rust_packages.rb
@@ -0,0 +1,2 @@
+system('rustup component add rustfmt clippy rust-src rust-analysis rls')
+system('cargo install cargo-tree')
