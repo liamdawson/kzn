dc84170ad8386f9099c5bf8c0af8059bd7468bee
Liam Dawson <liam@ldaws.com>
2019-03-27 16:55:59 +1100

    add cargo-edit

--- config/sub/rust_packages.rb
+++ config/sub/rust_packages.rb
@@ -2,4 +2,4 @@ def cargo_binary(name)
   system("command -v #{name} >/dev/null") || system("cargo install #{name}")
 end
 system('rustup component add rustfmt clippy rust-src rust-analysis rls')
-%w[cargo-tree].each { |name| cargo_binary(name) }
+%w[cargo-tree cargo-edit].each { |name| cargo_binary(name) }
