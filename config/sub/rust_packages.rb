706bdcfa6ccf57e359e1d86cc5d85b37d70193b8
Liam Dawson <liam@ldaws.com>
2019-04-11 19:58:02 +1000

    force install for cargo-edit

--- config/sub/rust_packages.rb
+++ config/sub/rust_packages.rb
@@ -1,5 +1,5 @@
 def cargo_binary(name)
-  system("command -v #{name} >/dev/null") || system("cargo install #{name}")
+  system("command -v #{name} >/dev/null") || system("cargo install --force #{name}")
 end
 system('rustup component add rustfmt clippy rust-src rust-analysis rls')
 %w[cargo-tree cargo-edit cargo-vendor].each { |name| cargo_binary(name) }
