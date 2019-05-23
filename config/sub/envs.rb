f17c8a0d11391de15bf13507e34b0e04090fdc72
Liam Dawson <liam@ldaws.com>
2019-05-23 23:23:20 +1000

    change envs config

--- config/sub/envs.rb
+++ config/sub/envs.rb
@@ -5,8 +5,18 @@ installs = [
   -> { system('nodenv install -s 10.15.0') },
   -> { system('nodenv install -s 11.6.0') },
   -> { system('goenv install -s 1.11.4') },
-  -> { system('rustup update') },
+  -> { system('rustup update') }
 ]
+
+plugins = {
+  '$(pyenv root)/plugins' => {
+    'pyenv-implicit' => 'https://github.com/concordusapps/pyenv-implict.git',
+    'pyenv-default-packages' => 'https://github.com/jawshooah/pyenv-default-packages.git'
+  },
+  '$(rbenv root)/plugins' => {
+    'rbenv-chef-workstation' => 'https://github.com/docwhat/rbenv-chef-workstation.git'
+  }
+}
 
 if installs.all?(&:call)
   system('rbenv global 2.6.2')
@@ -15,8 +25,16 @@ if installs.all?(&:call)
   system('goenv global 1.11.4')
   system('rustup default stable')
 
-  system('bash -c "git clone git://github.com/concordusapps/pyenv-implict.git $(pyenv root)/plugins/pyenv-implict"')
-  system('bash -c "git clone https://github.com/jawshooah/pyenv-default-packages.git $(pyenv root)/plugins/pyenv-default-packages"')
+  plugins.each_key do |plugin_dir|
+    system("mkdir -p #{plugin_dir}")
+
+    plugins[plugin_dir].each do |name, remote|
+      dest = File.join(plugin_dir, name)
+      next if Dir.exist?(dest)
+
+      system("bash -c 'git clone \"#{remote}\" \"#{dest}\"'")
+    end
+  end
 
   require_relative './rust_packages'
 else
