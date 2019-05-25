27014a1de717d9a95e8973a7e2e49aa031672dbe
Liam Dawson <liam@ldaws.com>
2019-05-25 14:36:14 +1000

    install chef workstation

--- build/Linux.rb
+++ build/Linux.rb
@@ -48,12 +48,12 @@ def commands
   [
     sys("sudo dnf upgrade -y"),
     sys("sudo dnf install -y dnf-plugins-core"),
-    sys("sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-#{`rpm -E %fedora`.chomp}.noarch.rpm", !File.exist?("/etc/yum.repos.d/rpmfusion-free.repo")),
-    sys("sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-#{`rpm -E %fedora`.chomp}.noarch.rpm", !File.exist?("/etc/yum.repos.d/rpmfusion-nonfree.repo")),
+    *repo_packages.map { |key, val| sys("sudo dnf install -y #{val}", !File.exist?(key)) },
     *coprs.map {|copr| sys("sudo dnf copr enable -y #{copr}")},
     *repos.map {|repo| sys("sudo dnf config-manager --add-repo #{repo}")},
     sys("sudo yum groupinstall -y #{software_groups.join(" ")}"),
     sys("sudo dnf install -y #{packages.join(" ")}"),
+    *remote_packages.map { |check, pkg| sys("sudo dnf install -y #{pkg}", !File.exist?(check)) },
     sys('[[ -d /snap ]] || sudo ln -s /var/lib/snapd/snap /snap'),
     *snaps.map { |snap| sys("sudo snap install #{snap}") }
   ].compact
@@ -67,6 +67,19 @@ def repos
   %w[
     https://download.docker.com/linux/fedora/docker-ce.repo
   ]
+end
+
+def repo_packages
+  {
+    '/etc/yum.repos.d/rpmfusion-free.repo' => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-#{`rpm -E %fedora`.chomp}.noarch.rpm",
+    '/etc/yum.repos.d/rpmfusion-nonfree.repo' => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-#{`rpm -E %fedora`.chomp}.noarch.rpm",
+  }
+end
+
+def remote_packages
+  {
+    '/opt/chef-workstation' => 'https://packages.chef.io/files/stable/chef-workstation/0.2.53/el/7/chef-workstation-0.2.53-1.el6.x86_64.rpm'
+  }
 end
 
 def command_exists(cmd)
