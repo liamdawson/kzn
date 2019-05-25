c0882d004b7ed98ac6fb8e216c043b8ec69e9d47
Liam Dawson <liam@ldaws.com>
2019-05-25 13:24:19 +1000

    don't reinstall cargo-edit every time

--- config/sub/rust_packages.rb
+++ config/sub/rust_packages.rb
@@ -1,5 +1,14 @@
-def cargo_binary(name)
-  system("command -v #{name} >/dev/null") || system("cargo install --force #{name}")
+def cargo_binary(name, check_for: nil)
+  check_for ||= name
+  system("command -v #{check_for} >/dev/null") || system("cargo install #{name}")
 end
 system('rustup component add rustfmt clippy rust-src rust-analysis rls')
-%w[cargo-tree cargo-edit cargo-vendor].each { |name| cargo_binary(name) }
+
+%w[
+  cargo-tree
+  cargo-vendor
+].each { |name| cargo_binary(name) }
+
+{
+  'cargo-edit': 'cargo-add'
+}.each { |key, val| cargo_binary(key, check_for: val) }
