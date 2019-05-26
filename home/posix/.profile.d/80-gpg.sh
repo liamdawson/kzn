e1ab7b5999b43227c19e329bfa41af9ac4cac808
Liam Dawson <liam@ldaws.com>
2019-05-26 11:47:52 +1000

    apparently fedora looks through .profile for SSH_AUTH_SOCK

--- home/posix/.profile.d/80-gpg.sh
+++ home/posix/.profile.d/80-gpg.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
 export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
 
 GPG_PATH="$(command -v gpg 2>/dev/null)"
 GPG2_PATH="$(command -v gpg2 2>/dev/null)"
 
-if [[ ! -z "${GPG2_PATH}" ]] && [[ ! -z "${GPG2_PATH}" ]]
+if [[ ! -z "${GPG2_PATH}" ]] && [[ ! -z "${GPG_PATH}" ]]
 then
   alias gpg="${GPG2_PATH}"
 fi
+
