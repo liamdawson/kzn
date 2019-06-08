0e3b4faa8108fd04a4375d88f3e0299b12210c10
Liam Dawson <liam@ldaws.com>
2019-06-08 20:30:06 +1000

    extend fedora bootstrap

--- misc/fedora-bootstrap.sh
+++ misc/fedora-bootstrap.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 sudo dnf install -y @development-tools curl file git libxcrypt-compat
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+mkdir ~/.kzn
+curl -sSL https://github.com/liamdawson/kzn/archive/master.tar.gz | tar -xz --directory="$HOME/.kzn" --strip-components=1
+~/.kzn/kzn cycle
