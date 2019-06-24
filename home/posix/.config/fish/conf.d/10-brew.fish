8d636e19ef5a6015df6910bc5e22ece89f7c8b89
Liam Dawson <liam@ldaws.com>
2019-06-24 14:38:32 +1000

    don't set manpath on macos

--- home/posix/.config/fish/conf.d/10-brew.fish
+++ home/posix/.config/fish/conf.d/10-brew.fish
@@ -9,15 +9,9 @@ end
 if set -q HOMEBREW_PREFIX
   set -x HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
   set -x HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
-  set -x PATH "$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
-  if set -q MANPATH
+  if [ ! -x "/usr/libexec/path_helper" ]
+    set -x PATH "$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
     set -x MANPATH "$HOMEBREW_PREFIX/share/man:$MANPATH"
-  else
-    set -x MANPATH "$HOMEBREW_PREFIX/share/man"
-  end
-  if set -q INFOPATH
     set -x INFOPATH "$HOMEBREW_PREFIX/share/info:$INFOPATH"
-  else
-    set -x INFOPATH "$HOMEBREW_PREFIX/share/info"
   end
 end
